89a1f3e6bdb2ebc54db88c3e276822a1292feeaa
Ian_Edwards <edwards.ian.c@gmail.com>
2022-03-03 12:06:25 -0600

    Phase 3: USER Auth complete

--- backend/command.md
+++ backend/command.md
@@ -1,1 +1,13 @@
 npx dotenv sequelize db:create
+
+npx sequelize model:generate --name User --attributes username:string,email:string,hashedPassword:string
+npx dotenv sequelize db:migrate
+
+//if you need to undue a single migration
+npx dotenv sequelize db:migrate:undo
+
+//user seed
+npx sequelize seed:generate --name demo-user
+
+npx dotenv sequelize db:seed:all
+npx dotenv sequelize db:seed:undo:all
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,42 @@
 const router = require("express").Router();
 
+//test tokens DB and router
 router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
 });
+
+// GET /api/set-token-cookie = test
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+// GET /api/restore-user = test
+const { restoreUser } = require('../../utils/auth.js');
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+// GET /api/require-auth = test
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 module.exports = router;
