791abb74fc43dcdc28ca9661d336dbbafa18eb74
Ian_Edwards <edwards.ian.c@gmail.com>
2022-03-10 13:11:40 -0600

    review form set-up

--- frontend/src/components/ReviewsAddForm/index.js
+++ frontend/src/components/ReviewsAddForm/index.js
@@ -1,147 +1,107 @@
-
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect, useHistory } from "react-router-dom";
+import { Redirect, useHistory, useParams } from "react-router-dom";
 import { ValidationError } from "../utils/validationError";
 import ErrorMessage from "../utils/ErrorMessage";
-import { createSpot } from "../../store/spots";
+import { getOneSpot } from "../../store/spots";
+
 import * as sessionActions from "../../store/session";
 
 function ReviewAddForm() {
-  const dispatch = useDispatch();
-  const sessionUser = useSelector((state) => state.session.user);
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [country, setCountry] = useState("");
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [image, setImage] = useState("");
-  const [errors, setErrors] = useState([]);
-  const [errorMessages, setErrorMessages] = useState({});
-  let history = useHistory();
+    const spotParamObj = useParams();
+    const spotId = spotParamObj.id;
+    const spot = useSelector((state) => state.spots[spotId]);
 
-  useEffect(() => {
-    const validationErrors = [];
-    if (address.length > 255)
-      return validationErrors.push(
-        "Please update address to less than 255 characters"
-      );
-    if (city.length > 40)
-      return validationErrors.push("Please limit city to 40 characters");
-    if (country.length > 80)
-      return validationErrors.push("Please limit country to 80 characters");
-    if (name.length > 100)
-      return validationErrors.push("Please limit name to 100 characters");
-    setErrors(validationErrors);
-  }, [address, city, country, name]);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getOneSpot(spotId));
+    }, [dispatch]);
+    const sessionUser = useSelector((state) => state.session.user);
+    const [rating, setRating] = useState(5);
+    const [review, setReview] = useState("");
+    const [errors, setErrors] = useState([]);
+    const [errorMessages, setErrorMessages] = useState({});
+    let history = useHistory();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const roundedPrice = Math.round(price * 100) / 100;
-    const priceCheck = (typeof(roundedPrice))
-    console.log(priceCheck);
-    const newSpot = {
-      userId: sessionUser.id,
-      address,
-      city,
-      country,
-      name,
-      price: roundedPrice,
-      mainImageURL: image,
+    useEffect(() => {
+      const validationErrors = [];
+      if (review.length > 255)
+        validationErrors.push(
+          "Please update review to less than 255 characters"
+        );
+      if (rating > 10 || rating < 1)
+         validationErrors.push("Please limit rating to between 1 and 10");
+      setErrors(validationErrors);
+    }, [review, rating]);
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      const review = {
+        id: spotId,
+        userId: sessionUser.id,
+        rating,
+        // review,
+      };
+      console.log(review);
+      let addedReview;
+      try {
+        // editedSpot = await dispatch(updateSpot(updatedSpot));
+        console.log("success");
+      } catch (error) {
+        console.log("there was an error in the edit spot form");
+      }
+      if (addedReview) {
+        setErrorMessages({});
+        setErrors([]);
+        history.push(`/spots/${spot?.id}`);
+      }
     };
-    //need to refactor the spot model to include a home image, then use the images table as secondary images
-    console.log(newSpot);
-    let createdSpot;
-    try {
-      createdSpot = await dispatch(createSpot(newSpot));
-    } catch (error) {
-     console.log("there was an error in the handle submit spot form")
-     history.push("/spots/add")
-    }
-    if(createSpot){
-      setErrorMessages({});
-      setErrors([]);
-      history.push("/spots");
-    }
 
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => (
-          <li key={idx}>{error}</li>
-        ))}
-      </ul>
-      <div>
-        <ErrorMessage message={errorMessages.overall} />
-      </div>
-      <div className="sign-up-form">
-        <label className="signup-form-label">
-          <input
-            type="text"
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </label>
-        <label className="signup-form-label">
-          <input
-            type="number"
-            placeholder="Add Price"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-            required
-          />
-        </label>
-        <label className="signup-form-label">
-          <input
-            type="text"
-            placeholder="image url"
-            value={image}
-            onChange={(e) => setImage(e.target.value)}
-            required
-          />
-        </label>
-        <label className="signup-form-label">
-          <input
-            type="text"
-            placeholder="Address"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-            required
-          />
-        </label>
-        <label className="signup-form-label">
-          <input
-            type="text"
-            placeholder="City"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            required
-          />
-        </label>
-        <label className="signup-form-label">
-          <input
-            type="text"
-            placeholder="Country"
-            value={country}
-            onChange={(e) => setCountry(e.target.value)}
-            required
-          />
-        </label>
-
-        <button
-          type="submit"
-          disabled={errors.length > 0}
-          className="btn-Log-in-Log-out"
-        >
-          Create
-        </button>
-      </div>
-    </form>
-  );
-}
+    return (
+      <>
+        <img src={`${spot?.mainImageURL}`}></img>
+        <h2>{`${spot?.name}`}</h2>
+        <form onSubmit={handleSubmit}>
+          <ul>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </ul>
+          <div>
+            <ErrorMessage message={errorMessages.overall} />
+          </div>
+          <div className="sign-up-form">
+          <label className="signup-form-label">
+              <div className="signup-form-text">Rating</div>
+              <input
+                type="number"
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
+                required
+              />
+            </label>
+            <label className="signup-form-label">
+              <div className="signup-form-text">Review</div>
+              <input
+                type="text"
+                placeholder="Please let us know what you think"
+                value={review}
+                onChange={(e) => setReview(e.target.value)}
+                required
+              />
+            </label>
+            <button
+              type="submit"
+              disabled={errors.length > 0}
+              className="btn-Log-in-Log-out"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </>
+    );
+  }
 
 export default ReviewAddForm;
